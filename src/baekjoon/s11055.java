86321a0ffba1dacd98407e74615edd020b7dc31b
DESKTOP-BBQAPPJ\user <wltjs188@gmail.com>
2021-03-26 00:13:40 +0900

    다이나믹 - 가장 큰 증가 부분 수열

---
+++ src/baekjoon/s11055.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class s11055 {
+    static int n;
+    static int[] list;
+    static int[] d;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        list = new int[n];
+        d = new int[n];
+        for(int i=0;i<n;i++){
+            list[i] = sc.nextInt();
+        }
+        System.out.println(go(n));
+    }
+
+    public static int go(int n) {
+        d[0] = list[0];
+        for(int i=1;i<n;i++){
+            d[i] = list[i];
+            for(int j=i-1;j>=0;j--){
+                if(list[j] < list[i]){
+                    int temp = d[j] + list[i];
+                    if(temp > d[i]){
+                        d[i] = temp;
+                    }
+                }
+            }
+        }
+        Arrays.sort(d);
+        return d[d.length-1]; //최대값
+    }
+}
